3f3f049e690c1f08fbc704b8cccc1cad5692c06c
Henryle-hd <henrydionizi@gmail.com>
2024-12-06 02:14:25 +0300

    ui

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -1,5 +1,4 @@
 import AudioCreated from "@/components/AudioCreated";
-// import SystemOutput from "@/components/SystemOutput";
 import UserArg from "@/components/UserArg";
 
 export default function Home() {
@@ -7,7 +6,6 @@ export default function Home() {
     <div className="min-h-screen bg-gradient-to-r from-blue-100 via-white to-purple-100 flex flex-col items-center justify-center">
       <AudioCreated />
       <UserArg />
-      {/* <SystemOutput /> */}
     </div>
   );
 }
